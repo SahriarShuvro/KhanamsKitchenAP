e91ff864eb41f12092255341545156ef90c6cfc6
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-07-30 12:25:56 +0600

    Add files via upload

--- js/mealonof.js
+++ js/mealonof.js
@@ -1,37 +1,39 @@
 {
     {
-        let disableCheck = document.querySelectorAll('.check-dis');
+        let disableCheck = document.querySelectorAll(".check-dis");
 
         const d = new Date();
         let day = d.getDate();
         for (let i = 0; i < 3; i++) {
-            day++
-            disableCheck[i].setAttribute("disabled", "disabled")
-        };
+            day++;
+            disableCheck[i].setAttribute("disabled", "disabled");
+        }
     } {
-        let disableCheck = document.querySelectorAll('.check-d');
+        let disableCheck = document.querySelectorAll(".check-d");
 
         const d = new Date();
         let day = d.getDate();
         for (let i = 0; i < 3; i++) {
-            day++
-            disableCheck[i].setAttribute("disabled", "disabled")
-        };
+            day++;
+            disableCheck[i].setAttribute("disabled", "disabled");
+        }
     }
 }
 
-
-
-
 {
     {
+        let dayOfMonth = [30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30]
         let d = new Date();
+        let month = dayOfMonth[d.getMonth()];
         let day = d.getDate() - 1;
+
         let dayShow = document.querySelectorAll(".p-d");
-        for (let i = 0; i < 31; i++) {
+        for (let i = 0; i <= month; i++) {
             day++
             dayShow[i].innerHTML = day;
-        };
+            if (day > month) {
+                day = 0
+            }
+        }
     }
-
 }
--- js/mealonof2.js
+++ js/mealonof2.js
@@ -1,9 +1,17 @@
 {
-    let d = new Date();
-    let day = d.getDate() - 1;
-    let dayShow = document.querySelectorAll(".p-dd");
-    for (let i = 0; i < 31; i++) {
-        day++
-        dayShow[i].innerHTML = day;
-    };
+    {
+        let dayOfMonth = [30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30]
+        let d = new Date();
+        let month = dayOfMonth[d.getMonth()];
+        let day = d.getDate() - 1;
+
+        let dayShow = document.querySelectorAll(".p-dd");
+        for (let i = 0; i <= month; i++) {
+            day++
+            dayShow[i].innerHTML = day;
+            if (day > month) {
+                day = 0
+            }
+        }
+    }
 }
