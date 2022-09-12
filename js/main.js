64cbcb7a0409d851d79bde3efa44bc69872c6c9e
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-09-13 02:16:24 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,146 +1,81 @@
-// Hamburger Menu Start
+// admin toggle 
 {
-    // Home Page Start 
-    const hamburger = document.querySelector(".hamburger");
-    const menu = document.querySelector(".main-menu");
-    const slideOff = document.getElementById("slider-id");
-    const disableMenu = document.getElementById("main-sub-menu");
+    let singTogl = document.getElementById('sing-in');
+    let adDrop = document.getElementById('adDrop');
 
-
-
-    hamburger.addEventListener("click", () => {
-        slideOff.classList.toggle("active-slideOff");
-        hamburger.classList.toggle("activeHamburger");
-    });
-
-    document.querySelectorAll(".menu-list").forEach(n => n.addEventListener("click", () => {
-        slideOff.classList.remove("active-slideOff");
-        hamburger.classList.remove("activeHamburger");
-    }));
-    hamburger.addEventListener("click", () => {
-        menu.classList.toggle("activeHamburger");
-    });
-    document.querySelectorAll(".menu-list").forEach(n => n.addEventListener("click", () => {
-        menu.classList.remove("activeHamburger");
-    }));
-    hamburger.addEventListener("click", () => {
-        disableMenu.classList.toggle("disableMenu");
-    });
-    // **********************
-    // **********************
-    // Home Page End
-    // Menu Page Start 
-    const swtichOff = document.getElementById("switch-box");
-
-
-    hamburger.addEventListener("click", () => {
-        swtichOff.classList.toggle("active-slideOff");
-    });
-    document.querySelectorAll(".menu-list").forEach(n => n.addEventListener("click", () => {
-        swtichOff.classList.remove("active-slideOff");
-    }));
+    singTogl.addEventListener('click', function() {
+        adDrop.classList.toggle('adminMenu');
+    })
 }
 
-// Hamburger Menu End
 
-
-
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
-
-// Jacascrpt for image slider manual navigation 
-var manualNav = function(manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-
-        btns.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-    });
-
-
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-}
-
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-    });
-});
-
-
-// Javascript for image slider autoplay navigation 
-
-var repeat = function(activeClass) {
-    let active = document.getElementsByClassName('active');
-    let i = 1;
-
-    var repeater = () => {
-        setTimeout(function() {
-            [...active].forEach((activeSlide) => {
-                activeSlide.classList.remove('active');
-            });
-
-
-            slides[i].classList.add('active');
-            btns[i].classList.add('active');
-            i++;
-
-            if (slides.length == i) {
-                i = 0;
-            }
-            if (i >= slides.length) {
-                return;
-            }
-            repeater();
-        }, 5000);
-    }
-    repeater();
-}
-repeat();
-
-
-// Menu Bar Stiky Start
-
-
-window.onscroll = function() { myFunction() };
-
-let navHeader = document.getElementById("menuBar");
-let container = document.getElementById("cntn-js");
-let stickyNav = navHeader.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > stickyNav) {
-        navHeader.classList.add("sticky");
-        container.classList.add("cntn-css");
-    } else {
-        navHeader.classList.remove("sticky");
-        container.classList.remove("cntn-css");
-    }
-};
-// Menu Bar Stiky End
-// Switch Section Start 
+// menu bar toggle
 {
-    let toggle = document.getElementById('switch');
-    let background = document.getElementById('switch-box');
-    let lunch = document.getElementById('menu-cards-sections-l');
-    let dinner = document.getElementById('menu-cards-sections-d');
-    let lunchDinner = document.getElementById('main-card-l-d');
+    let menuBarT = document.getElementById('d-menu');
+    let menuBarActive = document.getElementById('menuBarActive');
+    let menuB = document.getElementById('menuB');
 
+    menuBarT.addEventListener('click', function() {
+        menuBarActive.classList.toggle('menuToglActive');
+        menuB.classList.toggle('FmenuToglActive');
+    })
+}
 
-    let switchOn = toggle.onclick = function() {
-        toggle.classList.toggle('switchOn');
-        background.classList.toggle('backgroundSwitchOn');
-        lunch.classList.toggle('active-lunch');
-        dinner.classList.toggle('active-dinner');
-        lunchDinner.classList.toggle('lunchDinner');
-    };
-};
+// {
+//     window.onload = () => {
+//         const tab_switchers = document.querySelectorAll('[data-switcher]');
 
+//         for (let i = 0; i < tab_switchers.length; i++) {
+//             const tab_switcher = tab_switchers[i];
+//             const page_id = tab_switcher.dataset.tab;
 
+//             tab_switcher.addEventListener('click', () => {
+//                 document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+//                 tab_switcher.parentNode.classList.add('is-active');
 
+//                 SwitchPage(page_id);
+//             });
+//         }
+//     }
 
-// Switch Section End
+//     function SwitchPage(page_id) {
+//         console.log(page_id);
+
+//         const current_page = document.querySelector('.pages .page.is-active');
+//         current_page.classList.remove('is-active');
+
+//         const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+//         next_page.classList.add('is-active');
+//     }
+// }
+
+// Hamburger Toggle 
+{
+    let hamburger = document.getElementById('hamburger');
+    let menuBarHide = document.getElementById('menubar');
+
+    hamburger.addEventListener('click', function() {
+        menuBarHide.classList.toggle('activeHam');
+    })
+}
+
+// Image S
+{
+    function showPreview(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-1-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+} {
+    function showPreview2(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-2-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+}
