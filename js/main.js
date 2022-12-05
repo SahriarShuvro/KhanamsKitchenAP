f0a5b4c0eb1b6494363a5067c29a90933b3599c2
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-12-06 05:11:24 +0600

    Add files via upload
    Signed-off-by: SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,31 +1,103 @@
-let btnAdmin = document.getElementById('btnAdmin')
-let expandDiv = document.querySelector(".dropDownSection");
-btnAdmin.addEventListener('click', function() {
-    expandDiv.classList.toggle("active");
-})
+// admin toggle 
+{
+    let singTogl = document.getElementById('sing-in');
+    let adDrop = document.getElementById('adDrop');
+
+    singTogl.addEventListener('click', () => {
+        adDrop.classList.toggle('adminMenu');
+    })
+    document.addEventListener('click', event => {
+        if (event.target.closest('#sing-in')) return
+        adDrop.classList.remove('adminMenu');
+    })
+}
 
 
+// menu bar toggle
+{
+    let menuBarT = document.getElementById('d-menu');
+    let menuBarActive = document.getElementById('menuBarActive');
+    let menuB = document.getElementById('menuB');
 
-// NavBar Section
+    menuBarT.addEventListener('click', function() {
+        menuBarActive.classList.toggle('menuToglActive');
+        menuB.classList.toggle('FmenuToglActive');
+    })
+}
 
-let tapOpen = document.querySelector(".tapOpen");
-let tapClose = document.querySelector(".tapClose");
-let expandNavBar = document.querySelector(".navBarSection");
+// {
+//     window.onload = () => {
+//         const tab_switchers = document.querySelectorAll('[data-switcher]');
 
-tapOpen.addEventListener('click', function() {
-    tapOpen.classList.toggle('OpenBntActive')
-    tapClose.classList.toggle('active')
-    expandNavBar.classList.toggle("expandNavAction");
-})
-tapClose.addEventListener('click', function() {
-    tapOpen.classList.toggle("OpenBntActive");
-    tapClose.classList.toggle('active')
-    expandNavBar.classList.toggle("expandNavAction");
-})
+//         for (let i = 0; i < tab_switchers.length; i++) {
+//             const tab_switcher = tab_switchers[i];
+//             const page_id = tab_switcher.dataset.tab;
 
-// flatpicker 
-config = {
-    enableTime: true,
-    plugins: [new confirmDatePlugin({})],
-};
-flatpickr("input[type=datetime-local]", config);
+//             tab_switcher.addEventListener('click', () => {
+//                 document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+//                 tab_switcher.parentNode.classList.add('is-active');
+
+//                 SwitchPage(page_id);
+//             });
+//         }
+//     }
+
+//     function SwitchPage(page_id) {
+//         console.log(page_id);
+
+//         const current_page = document.querySelector('.pages .page.is-active');
+//         current_page.classList.remove('is-active');
+
+//         const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+//         next_page.classList.add('is-active');
+//     }
+// }
+
+// Hamburger Toggle 
+{
+    let hamburger = document.getElementById('hamburger');
+    let menuBarHide = document.getElementById('menubar');
+
+    hamburger.addEventListener('click', function() {
+        menuBarHide.classList.toggle('activeHam');
+    })
+}
+
+// Image S
+{
+    function showPreview(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-1-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+} {
+    function showPreview2(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-2-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+}
+
+// Expand
+{
+    let epandDO = document.getElementById('epandDO');
+    let epandDC = document.getElementById('epandDC');
+    let expandID = document.getElementById('expandID');
+
+    epandDC.addEventListener('click', () => {
+        expandID.classList.toggle('expandID');
+        epandDO.classList.toggle('epandDO');
+        epandDC.classList.toggle('epandDC');
+    })
+    epandDO.addEventListener('click', () => {
+        expandID.classList.toggle('expandID');
+        epandDC.classList.toggle('epandDC');
+        epandDO.classList.toggle('epandDO');
+    })
+}
